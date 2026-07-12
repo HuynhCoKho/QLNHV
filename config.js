b6081b7a474be1a09c1820094c106fe430039490
Cỏ Khô <92889822+HuynhCoKho@users.noreply.github.com>
2026-07-12 22:41:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Dán URL Web App (Google Apps Script) sau khi Deploy vào đây.
 // Ví dụ: https://script.google.com/macros/s/AKfycb.../exec
 // ============================================================
-const API_URL = "DÁN_URL_WEB_APP_APPS_SCRIPT_VÀO_ĐÂY";
+const API_URL = "https://script.google.com/macros/s/AKfycbwh_IqTPM6eKQNZfu9mN-M1qFXf2YlvicU8KaRdKaseGWt_0gaypSihHmh7bbUCRiDG/exec";
